20de5d683f8d54f9fcead73fd5fa52241787e15f
Zofia D <zszszs@example.com>
2022-04-26 18:32:02 +0200

    add getters and setters for surname and account number

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -7,9 +7,20 @@ public class Account {
     public String getName(){
         return name;
     }
-
     public void setName(String name){
-        this.name =name;
+        this.name = name;
+    }
+    public String getSurname(String surname){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public String getAccNumber(String accNumber){
+        return accNumber;
+    }
+    public void setAccNumber(String accNumber){
+        this.accNumber = accNumber;
     }
 
 }
